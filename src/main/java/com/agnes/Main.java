79e5b340d38d2e0232fce5ef4cea312c5902df2f
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-10 16:24:08 +0100

    Initial commit

---
+++ src/main/java/com/agnes/Main.java
@@ -0,0 +1,9 @@
+package com.agnes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my java basic practical page!");
+        Hello.main(args);
+
+    }
+}
