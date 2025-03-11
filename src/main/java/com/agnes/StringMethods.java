dcae7f578d21a60739c2a478e26198de98436bc7
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-11 14:01:36 +0100

    completed exercise 4

--- src/main/java/com/agnes/StringMethods.java
+++ src/main/java/com/agnes/StringMethods.java
@@ -1,19 +1,37 @@
 package com.agnes;
 
 public class StringMethods {
-    public static void main(String [] args) {
-        String name = "Java";
-        int length =name.length();
-        System.out.println("The length of Java is " +length);
+    public static void main(String[] args) {
 
-        String text = "Long Example Sentence" ;
+        // exercise 1
+        String name = "Java";
+        int length = name.length();
+        System.out.println("The length of Java is " + length);
+
+        //exercise 2
+        String text = "Long Example Sentence";
         char letter = text.charAt(6);
         System.out.println("The index position of 6 is " + letter);
 
-        String text1= "Even Long Example Sentence" ;
+        //exercise 3
+        String text1 = "Even Long Example Sentence";
         int index = text1.indexOf("o");
         System.out.println("The index position of O is " + index);
 
-}
+        //exercise 4
+        String text2 = "CAPS EQUALS SCREAMING";
+        text2=text2.toLowerCase();
+        System.out.println("Convert text to lowercase: " + text2);
+
+        // convert back to uppercase
+        String upperCase = text2.toUpperCase();
+        System.out.println("Convert text back to uppercase: " + upperCase);
+
+        //exercise 5
+
+
+
+
+    }
 
 }
