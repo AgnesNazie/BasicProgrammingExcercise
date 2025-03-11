5a5ac9c1f729920ce34c4adc91e217fb92514a10
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-11 13:46:06 +0100

    practicing String methods

--- src/main/java/com/agnes/ArithmeticOperations.java
+++ src/main/java/com/agnes/ArithmeticOperations.java
@@ -2,9 +2,19 @@ package com.agnes;
 
 public class ArithmeticOperations {
     public static void main(String[] args) {
+       //sum of two numbers 74,36
         int number = 74;
         int number1 =36;
         double sum =number + number1;
+        // print the result
         System.out.println(sum);
+
+        //division of two numbers 50, 3
+
+        double num = 50;
+        double num1 = 3;
+        double result =  num /num1;
+        System.out.println(result);
     }
+
 }
