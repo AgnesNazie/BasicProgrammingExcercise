8eb1924f9a933d71ef33e0e247af8945f2f2045e
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-12 10:55:09 +0100

    completed exercise 7,8 and 9

--- src/main/java/com/agnes/TimeConverter.java
+++ src/main/java/com/agnes/TimeConverter.java
@@ -1,12 +1,10 @@
 package com.agnes;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.DayOfWeek;
 
 public class TimeConverter {
-    public static void main (String []args) {
+    public static void main(String[] args) {
         //exercise 1
         LocalDate today = LocalDate.now();
         System.out.println(today);
@@ -20,7 +18,7 @@ public class TimeConverter {
 
         //exercise 3
         LocalDate lastMonday = today.with(DayOfWeek.MONDAY).minusWeeks(1);
-        for (int i = 0; i <7; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println(lastMonday.plusDays(i));
         }
         //exercise 4. parse a date from a string
@@ -30,15 +28,31 @@ public class TimeConverter {
         // output 2025-03-11
 
         //exercise 5 find the day of the week
-        LocalDate birthday = LocalDate.of (1995,8,15);
+        LocalDate birthday = LocalDate.of(1995, 8, 15);
         DayOfWeek dayOfWeek = birthday.getDayOfWeek();
-        System.out.println(birthday + " "  + dayOfWeek);
+        System.out.println(birthday + " " + dayOfWeek);
 
         //exercise 6
-        LocalDate  futureDate =LocalDate.now().plusYears(10).minusMonths(10);
+        LocalDate futureDate = LocalDate.now().plusYears(10).minusMonths(10);
         Month month = futureDate.getMonth();
-        System.out.println("Future date: "  + futureDate);
-        System.out.println("Extracted month: "  + month);
+        System.out.println("Future date: " + futureDate);
+        System.out.println("Extracted month: " + month);
 
+        //exercise 7
+        LocalDate birthDate = LocalDate.of(1995, 8, 15);
+        LocalDate futureDate1 = LocalDate.now().plusYears(10).minusMonths(10);
+        Period period = Period.between(birthDate, futureDate1);
+        System.out.println("Elapsed time: " + period.getYears() + " years, " + period.getMonths() + " months," + period.getDays() + " days");
+
+        // exercise 8
+        Period period1 = Period.of(4, 7, 29);
+        LocalDate futureDate2 = today.plus(period1);
+        System.out.println("New date after period: " + futureDate2);
+
+        //exercise 9
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentTime);
+
+        //exercise 10
     }
 }
