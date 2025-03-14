36bea2c80ce315a32be484e233d4732c59b25d66
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-14 13:55:48 +0100

    completed exercise 6

--- src/main/java/com/agnes/ArithmeticOperations.java
+++ src/main/java/com/agnes/ArithmeticOperations.java
@@ -7,30 +7,48 @@ public class ArithmeticOperations {
         int number1 =36;
         double sum =number + number1;
         // print the result
-        System.out.println(sum);
-
+        System.out.println("The sum is " + sum);
+        System.out.println("========================================");
         //division of two numbers 50, 3
 
         double num = 50;
         double num1 = 3;
         double result =  num /num1;
-        System.out.println(result);
+        System.out.println(" The division: " + result);
 
-        // exercise 4 arithmethic operations
+        System.out.println("===============================================");
+        // exercise 4 arithmetic operations
         double a = -5 + 8 * 6;
         double b = (55+9) % 9;
         double c = 20 + -3 * 5 / 8;
         double d = 5 + 15 / 3 * 2 - 8 % 3;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-
-        // excercise 5
+        System.out.println(" The Result is " + a);
+        System.out.println(" The Result is " + b);
+        System.out.println(" The Result is " + c);
+        System.out.println(" The Result is " + d);
+        System.out.println("==================================");
+        // exercise 5
         int firstNumber = 25;
         int secondNumber = 5;
         int product = firstNumber *secondNumber;
-        System.out.println(product);
+        System.out.println("The product: " + product);
+
+        // exercise 6 basic arithmetic operations
+        System.out.println("========================");
+        int x  = 125;
+        int y = 24;
+        int add = x + y;
+        int subtract = x - y;
+        int multiple = x * y;
+        double divide = x / y;
+        double remainder = x % y;
+        System.out.println("The sum is : " + add);
+        System.out.println("The subtraction  is : " + subtract);
+        System.out.println("The mul is : " + multiple);
+        System.out.println("The div is : " + divide);
+        System.out.println("The mod is : " + remainder);
+
+
 
     }
 
