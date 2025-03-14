8375e76b9a81067d4fc251d8374dea79b9aef565
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-14 15:44:34 +0100

    created a math operation class

--- src/main/java/com/agnes/ArithmeticOperations.java
+++ src/main/java/com/agnes/ArithmeticOperations.java
@@ -63,25 +63,7 @@ public class ArithmeticOperations {
         }
         scanner.close();
 
-        // exercise 9. expression evaluation
-        System.out.println("============================");
-        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
-
-        // exercise 10 computation
-
-        double input = 4.0;
-        input = input * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
-        System.out.println(input);
-        System.out.println("=========================");
-        //exercise 11. Circle: Area and Perimeter
-        double radius = 7.5;
-        double perimeters = 2 * Math.PI * radius;
-        double area = Math.PI * radius * radius;
-        System.out.println("The Perimeters is " + perimeters);
-        System.out.println("The area is " + area);
-
 
     }
-
 
 }
