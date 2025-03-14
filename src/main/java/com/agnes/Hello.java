ddce7f85ef8525f5d20853aea1e417c0218dde9c
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-14 14:49:49 +0100

    completed exercise 8

--- src/main/java/com/agnes/Hello.java
+++ src/main/java/com/agnes/Hello.java
@@ -5,6 +5,15 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Agnes Nazie");
         System.out.println("==========================");
+
+        // exercise 8. pattern display : Java
+
+        System.out.println("   J    a   v     v  a   ");
+        System.out.println("   J   a a   v   v  a a  ");
+        System.out.println("J  J  aaaaa   V V  aaaaa ");
+        System.out.println(" JJ  a     a   V  a     a");
+
+
     }
 }
 
